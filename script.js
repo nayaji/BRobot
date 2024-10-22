1cca9c0124a60f229d28615b8560fb2d3c6c2335
Najib Taiek <najtt89@gmail.com>
2024-10-22 04:53:05 +0200

    ok

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const form = document.querySelector('form');
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     console.log("Il n’y a pas eu de rechargement de page");
+
+
     const nom = document.getElementById("nom").value;
     const email = document.getElementById("email").value;
     const telephone = document.getElementById("telephone").value;
@@ -10,12 +13,19 @@ form.addEventListener("submit", (event) => {
     alert("Email: " + email);
     alert("telephone: " + telephone);
     alert("adresse: " + adresse);
+
+
 });
+
+
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     console.log("Il n’y a pas eu de rechargement de page");
+
+
     const email = document.getElementById("email").value;
     alert("Email: " + email);
+
 
 });
